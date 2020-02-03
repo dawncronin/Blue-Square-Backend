1cabc53ecd37a555e11be638f25c292c5b94ed91
dawncronin <github email address>
2020-02-03 09:28:31 -0800

    resorts data seeded

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -1,2 +1,14 @@
 class ResortsController < ApplicationController
+    skip_before_action :authorized
+
+
+    def index
+        resorts = Resort.all 
+        render json: ResortSerializer.new(resorts)
+    end
+
+    def show
+        resort = Resort.find(params[:id])
+        render json: ResortSerializer.new(resort)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# User.create(
-#     {username: "dawntime", 
-#     email: "dawncronin7@gmail.com", 
-#     password: "dawn", 
-#     password_confirmation: "dawn"})
 
 # Resort.create({
 #     name: "Kirkwood",
@@ -51,17 +41,17 @@ count = 0
 File.open("/Users/flatironschool/BlueSquare/BlueSquareBackend/BlueSquareAPI/Resorts.csv").each_line do |line|
     if count != 0 
         resort = line.split(/\t/)
-        if resort[23] === "TRUE"
+        if resort[23] == "TRUE"
             epic = true
         else 
             epic = false
         end
-        if resort[24] === "TRUE"
+        if resort[24] == "TRUE"
             ikon = true
         else 
             ikon = false
         end
-        if resort[25] === "TRUE"
+        if resort[25] == "TRUE\r\n" || resort[25] == "TRUE"
             begineer = true
         else 
             begineer = false
